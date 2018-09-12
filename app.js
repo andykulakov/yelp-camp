5bf6d1a62ba362a1d7eb7d39195c02c461cf23a0
Andrei Kulakov <andykulakov@gmail.com>
2018-09-12 09:27:00 +0000

    add navigation toggle

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var commentRoutes = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes = require('./routes/index');
 
-// mongoose.connect('mongodb://localhost/yelp_camp');
-mongoose.connect('mongodb://andy:andyjulia1@ds251902.mlab.com:51902/andykulakov-yelpcamp');
+mongoose.connect('mongodb://localhost/yelp_camp');
+// mongoose.connect('mongodb://andy:andyjulia1@ds251902.mlab.com:51902/andykulakov-yelpcamp');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
