77e957eabc51a1480892c7417c8606da3613304d
Andrei Kulakov <andykulakov@gmail.com>
2018-09-16 14:29:33 +0000

    add environment variable for database

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ var commentRoutes = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes = require('./routes/index');
 
-// mongoose.connect('mongodb://localhost/yelp_camp');
-mongoose.connect('mongodb://andy:andyjulia1@ds251902.mlab.com:51902/andykulakov-yelpcamp');
+var url = process.env.DATABASEURL || 'mongodb://localhost/yelp_camp';
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
